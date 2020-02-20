7590132293385d0f7395397fd721837737e60a73
Adrian Gomez <aegomez@tekmexico.com>
2020-02-20 12:31:11 -0800

    Update icons library import

--- src/app.tsx
+++ src/app.tsx
@@ -1,54 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faAngleDown,
-  faCheck,
-  faCircle,
-  faCircleNotch,
-  faEdit,
-  faEnvelope,
-  faEnvelopeOpen,
-  faExclamationTriangle,
-  faGlobe,
-  faLock,
-  faLongArrowAltLeft,
-  faPaperPlane,
-  faPlus,
-  faSearch,
-  faSignOutAlt,
-  faTrash,
-  faUser,
-  faUsers,
-  faWrench
-} from '@fortawesome/free-solid-svg-icons';
 
 import { store } from './store';
 import { RootRouter } from './router';
 
-// Build icons library
-library.add(
-  faAngleDown,
-  faCheck,
-  faCircle,
-  faCircleNotch,
-  faEdit,
-  faEnvelope,
-  faEnvelopeOpen,
-  faExclamationTriangle,
-  faGlobe,
-  faLock,
-  faLongArrowAltLeft,
-  faPaperPlane,
-  faPlus,
-  faSearch,
-  faSignOutAlt,
-  faTrash,
-  faUser,
-  faUsers,
-  faWrench
-);
+import './icons';
 
 // Main app
 const App: React.FC = () => (
---
+++ src/icons.ts
@@ -0,0 +1,49 @@
+// Import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Import icons
+import {
+  faAngleDown,
+  faCheck,
+  faCircle,
+  faCircleNotch,
+  faEdit,
+  faEnvelope,
+  faEnvelopeOpen,
+  faExclamationTriangle,
+  faGlobe,
+  faLock,
+  faLongArrowAltLeft,
+  faPaperPlane,
+  faPlus,
+  faSearch,
+  faSignOutAlt,
+  faTrash,
+  faUser,
+  faUsers,
+  faWrench
+} from '@fortawesome/free-solid-svg-icons';
+
+// Add icons to library
+// Build icons library
+library.add(
+  faAngleDown,
+  faCheck,
+  faCircle,
+  faCircleNotch,
+  faEdit,
+  faEnvelope,
+  faEnvelopeOpen,
+  faExclamationTriangle,
+  faGlobe,
+  faLock,
+  faLongArrowAltLeft,
+  faPaperPlane,
+  faPlus,
+  faSearch,
+  faSignOutAlt,
+  faTrash,
+  faUser,
+  faUsers,
+  faWrench
+);
